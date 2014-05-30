bac2db1d73586061a44ffe9d609d7ff98ce018f9
Jordan Santell <jsantell@gmail.com>
2014-05-30 14:11:13 -0400

    remove unused fft features

--- examples/FFT.js
+++ examples/FFT.js
@@ -61,8 +61,6 @@ function process (module) {
   }
   else if (module.type === 'time') {
     module.input.getByteTimeDomainData(data);
-    // Abort if no data coming through, quick hack, needs fixed
-    if (module.data[3] < 5) return;
     ctx.beginPath();
     ctx.moveTo(0, module.h / 2);
     for (var i= 0, l = data.length; i < l && i < module.count; i++) {
@@ -74,33 +72,4 @@ function process (module) {
     ctx.stroke();
     ctx.closePath();
   }
- 
-  if (!module.onBeat) return;
- 
-  var min = module.range[0];
-  var max = module.range[1];
-  var threshold = module.threshold;
-  var decay = module.decay;
-  var wait = module.wait;
-  var currentWait = module.currentWait || wait;
-  var current = reduceMagnitude(data, min, max, function (total, mag) {
-    return mag + total;
-  }, 0);
-
-  if (current > threshold) {
-    module.onBeat(current);
-    module.threshold = current;
-    module.currentWait = wait;
-  } else {
-    module.threshold -= decay;
-    module.currentWait -= 1;
-    if (module.offBeat) module.offBeat(current);
-  }
-}
-
-function reduceMagnitude (array, min, max, reducer, init) {
-  var aggregate = init;
-  for (var i = min; i < max + 1; i++)
-    aggregate = reducer(aggregate, array[i]);
-  return aggregate / MAX_UINT8 / (max - min + 1);
 }
